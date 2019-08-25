1f4f1350cbb21c74571391d9f653c5608a47c9b6
Johnder Baul <imjohnderbaul@gmail.com>
2019-08-25 18:19:35 +0800

    initial setup

---
+++ packages/native/src/App.js
@@ -0,0 +1,20 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+import test from '@space/core'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>{test}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
